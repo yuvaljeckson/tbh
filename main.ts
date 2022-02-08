78d75a4d4d8ded3bfa5e1289d88b4dc6e6dbcca7
yuvaljeckson <99249353+yuvaljeckson@users.noreply.github.com>
2022-02-08 11:22:31 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    basic.showIcon(IconNames.Square)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.SmallSquare)
+    basic.showIcon(IconNames.Square)
+})
