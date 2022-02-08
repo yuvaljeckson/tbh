8a60c239ffa305c90a54b52d0b3c5399c3d15f50
yuvaljeckson <99249353+yuvaljeckson@users.noreply.github.com>
2022-02-08 11:38:25 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,100 @@
 basic.forever(function () {
-    basic.showIcon(IconNames.Square)
-    basic.showIcon(IconNames.SmallSquare)
     basic.showLeds(`
-        . . . . .
-        . . . . .
-        . . # . .
-        . . . . .
-        . . . . .
+        . # # # .
+        # # # . .
+        # # . . .
+        # # # . .
+        . # # # .
         `)
-    basic.showIcon(IconNames.SmallSquare)
-    basic.showIcon(IconNames.Square)
+    basic.showLeds(`
+        . . # # .
+        . # # # #
+        . # # . .
+        . # # # #
+        . . # # .
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # # #
+        . . # # #
+        . . # # #
+        . . . # #
+        `)
+    basic.showLeds(`
+        . . . . #
+        # . . # #
+        # . . # #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . .
+        . # . . #
+        # # . . #
+        # # . . #
+        # . . . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # . # . .
+        # # # . .
+        # # # . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        . # . # .
+        # # # # .
+        # # # # .
+        # . # . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # # # #
+        # # # # #
+        . # . # .
+        `)
+    basic.showLeds(`
+        . . # # #
+        . # . # .
+        . # # # #
+        . # # # #
+        . . # . #
+        `)
+    basic.showLeds(`
+        . . . # #
+        . . # . #
+        . . # # #
+        . . # # #
+        . . . # .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . . # #
+        . . . # #
+        . . . . #
+        `)
+    basic.showLeds(`
+        # . . . .
+        . . . . #
+        . . . . #
+        . . . . #
+        # . . . .
+        `)
+    basic.showLeds(`
+        # # . . .
+        # . . . .
+        . . . . .
+        # . . . .
+        # # . . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # # . . .
+        # . . . .
+        # # . . .
+        # # # . .
+        `)
 })
